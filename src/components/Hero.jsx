c730e32b7ea59813ce135c3f6384746ad4570966
Mario-SO <gh_mario.proclaim@slmail.me>
2022-08-14 18:08:19 +0200

    Kinda responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 export function Hero() {
     return (
-        <div class="hero min-h-screen max-w-screen">
+        <div class="hero lg:min-h-screen">
             <div class="hero-content flex-col lg:flex-row">
                 <div>
-                    <h1 class="text-8xl font-bold">UNPOLITE COLLECTIONS</h1>
+                    <h1 class="font-bold lg:text-8xl md:text-md sm:text-sm">UNPOLITE COLLECTIONS</h1>
                 </div>
                 <button class="text-8xl bg-info">🖕</button>
             </div>
