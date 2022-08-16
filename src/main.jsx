997e7dfa7c96c689730c39acb7d31619b4a037b1
Mario-SO <gh_mario.proclaim@slmail.me>
2022-08-16 17:19:32 +0200

    Router now working

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import './index.css'
 
+// Routes
+import App from './App' 
+import Hello from './routes/hello'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      {/* <Route path="/hello" element={<Hello />} /> */}
+    </Routes>
+  </BrowserRouter>
 )
